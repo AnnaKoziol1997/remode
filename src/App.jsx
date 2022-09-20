72c290e31aafb73fe8f9ce6cbc2cacc4843c22c8
AnnaKoziol1997 <85227390+AnnaKoziol1997@users.noreply.github.com>
2022-09-20 10:40:55 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Komponent from "./Komponent";
+import "./index.css";
+
+const App = () => (
+<Komponent/>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/Komponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Button} from "@mui/material"
+
+const Komponent =()=>{
+    return(
+        <Button>
+            TEST
+        </Button>
+    )
+
+}
+
+export default Komponent;
