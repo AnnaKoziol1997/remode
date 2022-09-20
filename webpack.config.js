f06ed8783a9a3697f05aea5cdd3c60d2601ff994
AnnaKoziol1997 <85227390+AnnaKoziol1997@users.noreply.github.com>
2022-09-20 12:41:08 +0200

    push 1

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:2222/",
+    publicPath: "https://remotemf-3c051.web.app/",
   },
 
   resolve: {
